5ebae9fd6d83348b98d0e8c0434c4564eafc7005
manu <manuthoen@gmail.com>
2023-07-16 23:47:04 +0200

    Add possibility to show/hide by plugin name. Permission nodes: commandcentral.hideplugin.<PluginName>, commandcentral.showplugin.<PluginName> (e.g. commandcentral.showplugin.Essentials)

--- src/main/java/de/ironaxe/commandcentral/Filter/FilterCommand.java
+++ src/main/java/de/ironaxe/commandcentral/Filter/FilterCommand.java
@@ -3,10 +3,17 @@ package de.ironaxe.commandcentral.Filter;
 public class FilterCommand {
     private String command;
     private String permission;
+    private String pluginName;
 
     public FilterCommand(String command, String permission) {
         this.command = command;
         this.permission = permission;
+    }    
+
+    public FilterCommand(String command, String permission, String pluginName) {
+        this.command = command;
+        this.permission = permission;
+        this.pluginName = pluginName;
     }    
 
     public String getPermission() {
@@ -15,5 +22,9 @@ public class FilterCommand {
 
     public String getCommand() {
         return command;
+    }
+
+    public String getPluginName() {
+        return pluginName;
     }
 }
