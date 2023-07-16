5a1ccbad6fb504af58065f16e82ae50e061ab230
manu <manuthoen@gmail.com>
2023-07-16 12:34:25 +0200

    Added github action workflow, always recreate docker containers

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 mvn -Duser.name=build clean package
-docker-compose restart -t 0
-
+docker-compose up --force-recreate -t 0 -d
 if [ $# -gt 0 ]
   then
     docker attach commandcentral_$1_1
