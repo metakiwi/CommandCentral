86046827951b7d634bb3be80f10341a2607c4a11
manu <manuthoen@gmail.com>
2023-07-19 11:49:57 +0200

    Add bStats implementation

--- src/main/java/de/ironaxe/commandcentral/CommandCentralBungee.java
+++ src/main/java/de/ironaxe/commandcentral/CommandCentralBungee.java
@@ -1,11 +1,15 @@
 package de.ironaxe.commandcentral;
 
+import org.bstats.bungeecord.Metrics;
+
 import de.ironaxe.commandcentral.Listener.ProxyDefineCommandsEventListener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class CommandCentralBungee extends Plugin {
     @Override
     public void onEnable() {
+        new Metrics(this, 19145);
+
         getProxy().getPluginManager().registerListener(this, new ProxyDefineCommandsEventListener(this));
     }
 
